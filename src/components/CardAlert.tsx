57e585005c000ec2b8a50ffa877e320b86906e98
guilhermeKramer <guilherme_ok@hotmail.com>
2024-10-21 23:49:13 -0300

    first commit

---
+++ src/components/CardAlert.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
+
+export default function CardAlert() {
+  return (
+   <></>
+  );
+}
---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Avatar from '@mui/material/Avatar';
+import MuiDrawer, { drawerClasses } from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import SelectContent from './SelectContent';
+import MenuContent from '../components/MenuContent';
+import CardAlert from '../components/CardAlert';
+import OptionsMenu from '../components/OptionsMenu';
+
+const drawerWidth = 240;
+
+const Drawer = styled(MuiDrawer)({
+  width: drawerWidth,
+  flexShrink: 0,
+  boxSizing: 'border-box',
+  mt: 10,
+  [`& .${drawerClasses.paper}`]: {
+    width: drawerWidth,
+    boxSizing: 'border-box',
+  },
+});
+
+export default function SideMenu() {
+  return (
+    <></>
+  );
+}
